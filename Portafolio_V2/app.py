d3296a31449f00fc720dfb171fdf9e9d3da3dca2
yonatanleyton4-gif <yonatanleyton4@gmail.com>
2026-02-14 19:53:45 -0500

    Organizando templates y static

--- Portafolio_V2/app.py
+++ Portafolio_V2/app.py
@@ -2,14 +2,10 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    proyectos_web =[
-        {"nombre": "Portafolio V2", "tech": "flask & Python","status":"En proceso"},
-        {"nombre": "Tienda Mooca", "tech": "JS & Python","status":"Pendiente"},
-        {"nombre": "App Clima", "tech": "APIs","status":"pendiente"}
-    ]
-    return render_template("index.html", proyectos=proyectos_web)
+@app.route('/')
+def index():
+    # Flask buscará automáticamente en la carpeta 'templates'
+    return render_template('index.html')
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
