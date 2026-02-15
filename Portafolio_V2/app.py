7d2ecabdf962b2aa13cd7d6f4e7e93570f44518e
yonatanleyton4-gif <yonatanleyton4@gmail.com>
2026-02-14 19:56:34 -0500

    Sistema de rutas para múltiples páginas

--- Portafolio_V2/app.py
+++ Portafolio_V2/app.py
@@ -2,10 +2,26 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# Ruta para el Inicio
 @app.route('/')
-def index():
-    # Flask buscará automáticamente en la carpeta 'templates'
-    return render_template('index.html')
+def home():
+    # Buscamos el index dentro de la carpeta INICIO
+    return render_template('INICIO/index.html')
+
+# Ruta para Proyectos
+@app.route('/proyectos')
+def proyectos():
+    return render_template('PROYECTOS/index.html')
+
+# Ruta para Sobre Mí
+@app.route('/sobre-mi')
+def sobre_mi():
+    return render_template('SOBRE MI/index.html')
+
+# Ruta para Contacto
+@app.route('/contacto')
+def contacto():
+    return render_template('CONTACTO/index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
